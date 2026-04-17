c6860a5b34e614040fea67288c7f8f6ad0bbec02
Qurban <qurbanhanif120@gmail.com>
2026-04-17 22:48:41 +0500

    changes updated in controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './features/auth/presentation/decorators/public.decorator';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return 'Enterprise CRM & Tasks Platform API is running...';
